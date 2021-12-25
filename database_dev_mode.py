1f296ed9caeceb572f2dd721d7934f703a176567
Rashawn <arctitan78@gmail.com>
2021-12-25 12:06:22 -0500

    created the database_dev_mode.py file updated the process.txt file

---
+++ database_dev_mode.py
@@ -0,0 +1,3 @@
+### This code is under development ###
+# this is the database_dev_mode.py file that builds the transactions_dev_mode.db database
+print("test")
--- ex_app_dev_mode.py
+++
@@ -1,3 +0,0 @@
-### This code is under development ###
-# this is the ex_app.py file that builds the transaction.db database
-print("test")
