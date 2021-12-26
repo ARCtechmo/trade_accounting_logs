6bc7c9a1cf802bcef88388195c72e37e02224bf9
Rashawn <arctitan78@gmail.com>
2021-12-26 10:34:10 -0500

    wrote the starter code to form the transaction.db database created the first three tables activity_log, brokers, time_log modified the transactions_db_diagram to replace "foreign_key" with "primary_key" in the time_log table

--- database_dev_mode.py
+++ database_dev_mode.py
@@ -1,3 +1,29 @@
 ### This code is under development ###
 # this is the database_dev_mode.py file that builds the transactions_dev_mode.db database
-print("test")
+import sqlite3
+# conn = sqlite3.connect(":memory:")
+conn = sqlite3.connect('transactions.db')
+cur = conn.cursor()
+# cur.execute("DROP TABLE IF EXISTS time_log")
+cur.executescript('''
+CREATE TABLE IF NOT EXISTS brokers(
+    id INTEGER NOT NULL PRIMARY KEY UNIQUE,
+    broker TEXT NOT NULL UNIQUE
+    );
+
+CREATE TABLE IF NOT EXISTS activity_log(
+    id INTEGER NOT NULL PRIMARY KEY UNIQUE,
+    activity TEXT NOT NULL UNIQUE
+    );
+
+CREATE TABLE IF NOT EXISTS time_log(
+    start_time TEXT NOT NULL,
+    end_time TEXT NOT NULL,
+    activity_log_id INTEGER,
+    brokers_id INTEGER,
+    PRIMARY KEY(activity_log_id, brokers_id)
+    )
+''')
+conn.commit()
+print("---------------------table creation successful----------------------")
+conn.close()
