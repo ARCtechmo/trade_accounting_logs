a6c4479a873f24adf2548cabd4c8f12dac96b8b0
Rashawn <arctitan78@gmail.com>
2022-01-08 13:15:29 -0500

    continued work on data formatting; working on the dates in the list the dates are a mess from the .csv file so it requires a bit of work

--- fx_dev_mode.py
+++ fx_dev_mode.py
@@ -17,28 +17,26 @@ with open('fx_account_test.csv',newline='') as csvfile:
     next(csvfile) # removes the header - comment out if .csv file has no header
     for row in reader:
         if row[4] != "":
-            # print(row)
             fxlst.append(row)
-print("\n-----------test list ----------------------")
-# print(fxlst)
-print("\n-----------test list ----------------------\n")
+
+# fxlst is a list of trades only (no commission or fee data is included)
+# count rows of transaction trades only
 count = 0
 for item in fxlst:
     count +=1
-    # print(item)
-print("\n-------------test count rows---------------------")
-print(f'There are {count} returned rows with buy/sell data.\n')
+print(f'There are {count} returned rows with buy/sell data.')
 
-# alert the user when there are missing opening transaction ids
+# create a new list consisting of only the transaction ids with open dates only
 transfxlst = []
 count = 0
 for item in fxlst:
     if item[2] == '':
         item[2] = '000000000'
     transfxlst.append(item[3])
-    # print(f'close_id: {item[2]}, open_id: {item[3]}')
-print("\n-----------------opening transaction id-----------------")
-# print(transfxlst)
+
+# alert the user when there are missing opening transaction ids
+# create a dictionary to get transaction id numbers and the count
+# this will separate matching from missing id numbers
 di = dict()
 for num in transfxlst:
     di[num] = di.get(num,0) + 1
@@ -52,12 +50,13 @@ for key,value in di.items():
     else:
         matching_id = key
         matched_lst.append(matching_id)
-        print('open_transaction_id_matched:', matching_id)
-print("\n-------------------test: list of matched ids--------------------------")
+        # print('open_transaction_id_matched:', matching_id)
+print("-----------------missing opening transaction id-----------------\n")
+
+# matched_lst contains all transaction ids that have open and close matches
 # print(matched_lst)
 
-### fxlst is the list that contains the transactions but may have missing data
-# print(fxlst)
+# fxlst is the list that contains the transactions but may have missing data
 # for s in fxlst:
 #     print(s)
 
@@ -70,11 +69,8 @@ for row in fxlst:
     if open_id in matched_lst:
         fxlst2.append(row)
 
-### Refine fxlst2
 # fxlst2 contains matching opening transactions
-# extract dates from the rows that have the opening date and ...
-# ...insert into the rows
-# print(fxlst2)
+# extract dates from the rows that have the opening date and insert into the rows
 opentrans = []
 for item in fxlst2:
     # print(item)
@@ -91,10 +87,31 @@ for tup in opentrans:
     for x in fxlst2:
         if x[2] != '000000000' and tup[1] == x[3]:
             fxlst3.append([tup[0],x])
-# fxlst3 is an array witth the opening date,[column data as a list]
-for y in fxlst3:
-    print(y)
-
-### task: split the fxlst3 into a new lst; the goals is to get one list
 
 ### task: clean and format the data in each column
+# format the date YYY-MM-DD-HH:MM
+for y in fxlst3:
+    # print(y)
+    print(y[0])
+
+### START HERE NEXT ###
+### task: split the fxlst3
+## re-evaluated the fx data; *** the broker uses DD-MM-YYYY HH:MM:SS format
+# need to account for various date formats and length (see my excel sheet in projects folder on Windows)
+# 1) need to account for seconds HH:MM:SS
+# 2) there are many different combinations so the key is to differentiate by character
+# 3) DD/MM/YYYY   DD/M/YYYY  D/MM/YYYY
+# 4) if y[0][1] == '/' and y[0][5] == '/'    if y[0][2] == '/' and y[0][4] == '/'
+# if you do it this way the seconds should not matter becuase it captures the date correctly
+# do not set the conditions based on length (redo the code below)
+for y in fxlst3:
+    if len(y[0]) == 15:
+        entry_yr = y[0][5:10]
+        entry_mo = y[0][2:5]
+        entry_day = y[0][:1]
+        entry_time = y[0][8:]
+        formatted_date = f'{entry_yr}-{entry_mo}-{entry_day} {entry_time}'
+        print(formatted_date)
+
+
+## task: clean and format the data in each column
