6b9e6aff47e3a8193b42404fd1ec3704cb99d8ed
Rashawn <arctitan78@gmail.com>
2022-01-15 11:57:52 -0500

    fixed an error with the ordering of the transaction open ids; all rows open and close dates are properly matched; dates and times are are properly formatted; next, get the remaining items into the finalized list then test

--- fx_dev_mode.py
+++ fx_dev_mode.py
@@ -11,42 +11,47 @@ import re
 #     for row in reader:
 #         print(row[])
 
-# extract rows only with open and closed trades (e.g. no commission or financing rows)
-# append the data to a list where to do further cleaning
+# extract rows containing commission and financing
+# fxlst is a list of trades only (no commission or fee data is included)
 fxlst = []
 filename = input("enter the .csv filename: ")
+if filename == '':
+    filename = 'fx_oct_test.csv'
 with open(filename, newline='') as csvfile:
     reader = csv.reader(csvfile)
     # next(csvfile) # removes the header - comment out if .csv file has no header
     for row in reader:
         if row[4] != "":
             fxlst.append(row)
+# print(fxlst)
 
-# fxlst is a list of trades only (no commission or fee data is included)
 # count rows of transaction trades only
 count = 0
 for item in fxlst:
     count +=1
-print(f'There are {count} returned rows with buy/sell data.')
+# print(f'There are {count} returned rows with buy/sell data.')
 
-# create a new list consisting of only the transaction ids with open dates only
+# first step to match the open / closed transaction ids
+# transfxlst consists of only the open / closed transaction ids
 transfxlst = []
-count = 0
 for item in fxlst:
     if item[2] == '':
+        # insert 000000000 in field 2 to identify rows that have the entry times
         item[2] = '000000000'
     transfxlst.append(item[3])
+# print(transfxlst)
 
-# alert the user when there are missing opening transaction ids
-# create a dictionary to get transaction id numbers and the count
-# this will separate matching from missing id numbers
+# step to to match the open / closed transaction ids
+# dictionary gets a count of the transaction ids
+# matched_lst contains all transaction ids that have open and close ids
+matched_lst = []
 di = dict()
 for num in transfxlst:
     di[num] = di.get(num,0) + 1
 print("\n-----------------missing opening transaction id-----------------")
-matched_lst = []
 for key,value in di.items():
-    # print(k,v)
+
+    # alert the user when there are missing opening transaction ids
     if value <2:
         missing_id = key
         print('missing open transaction id data for: ', missing_id)
@@ -55,25 +60,27 @@ for key,value in di.items():
         matched_lst.append(matching_id)
         # print('open_transaction_id_matched:', matching_id)
 print("-----------------missing opening transaction id-----------------\n")
-
-# matched_lst contains all transaction ids that have open and close matches
 # print(matched_lst)
 
-# fxlst is the list that contains the transactions but may have missing data
-# for s in fxlst:
-#     print(s)
-
-# remove the missing data from fxlst
-# only include entry_date transaction_id to the matched_id in the match_lst
+# step 3 to match open / closed transaction ids
+# remove the missing transaction id identified in the dictionary from fxlst
+# fxlst2 contains rows only with open and closed /  open transactions
 fxlst2 = []
 for row in fxlst:
     open_id = row[3]
     # print(open_id)
     if open_id in matched_lst:
         fxlst2.append(row)
+# print("---------------fxlst2: open-close transactions--------------------")
+# for row in fxlst2:
+#     print(row)
+# print("---------------fxlst2: open-close transactions--------------------\n")
 
-# fxlst2 contains matching opening transactions
+
+# step 4 to match open / closed transaction ids
 # extract dates from the rows that have the opening date and insert into the rows
+# opentrans is a list of tuples with the open dates and open transaction ids
+# print("---------------tuples: opening: transaction ids--------------------")
 opentrans = []
 for item in fxlst2:
     # print(item)
@@ -82,14 +89,22 @@ for item in fxlst2:
         opentrans.append((entry_date, item[3]))
     else:
         pass
-# opentrans is a list of tuples with the entry dates and transaction ids
-# this extracts the columns with closing date '000000000'
+# print(opentrans)
+# print("---------------tuples: opening: transaction ids--------------------\n")
+
+# step 5 matches inserts the opening date to the row with the matching closed date
+# extract the columns with closing date '000000000'
+# fxlst3 contains all rows with the needed data now ready for formatting
+print("---------------fxlst3: matched open-close transactions--------------------")
 fxlst3 = []
-for tup in opentrans:
-    # print(tup)
-    for x in fxlst2:
-        if x[2] != '000000000' and tup[1] == x[3]:
+for x in fxlst:
+    for tup in opentrans:
+        if x[2] != '000000000' and x[3] == tup[1]:
             fxlst3.append([tup[0],x])
+
+for row in fxlst3:
+    print(row)
+print("---------------fxlst3: matched open-close transactions--------------------\n")
 
 # extract and format the entry date to YYYY-MM-DD-HH:MM
 fxlst4 = []
@@ -244,39 +259,49 @@ for dmy in fxlst3:
 # for item in fxlst5:
 #     print(item)
 
+# extract the currency pairs from fxlst3
 # marketlst contains the formatted market (e.g. EURUSD, AUDJPY, etc...)
 marketlst = []
-for mkt in fxlst2:
-    print(mkt)
-    if mkt[2] != '000000000':
-        market = mkt[1][4:]
-        market2 = re.split(' ',market)
-        market3 = market2[2]
-        marketlst.append(market3)
-# print(marketlst)
+for mkt in fxlst3:
+    market = mkt[1][1]
+    market2 = re.split(' ',market)
+    market3 = market2[3]
+    marketlst.append(market3)
 
-# extract the closing transaction ids
-print("\n------------closing transaction ids---------------------\n")
+# extract the closing transaction ids from fxlst3
+# close_id_lst contains the closing transaction ids
 close_id_lst = []
-for elements in fxlst2:
-    if elements[2] != '000000000':
-        close_id = elements[2]
-        close_id_lst.append(close_id)
-print(close_id_lst)
+for elements in fxlst3:
+    close_id = elements[1][2]
+    close_id_lst.append(close_id)
 
-# extract the closing transaction ids
-print("\n----------opening transaction ids---------------------\n")
+# extract the opening transaction ids from fxlst3
+# open_id_lst contains the opening transaction ids
 open_id_lst = []
-for elements in fxlst2:
-    if elements[2] != '000000000':
-        open_id = elements[3]
-        open_id_lst.append(open_id)
-print(open_id_lst)
+for elements in fxlst3:
+    open_id = elements[1][3]
+    open_id_lst.append(open_id)
 
-### START HERE NEXT ###
-# fxloglst will contain the finalized formatted list to imported into the app
-# the closing dates are not matched to the correct opening transaction id
-# there is something wrong about the order in which the dates are iterated and matched
+# fxloglst will contain the finalized formatted list to import into the app
+fxlog = []
 print("\n-----------test of complete fxloglst----------------")
-for entry, exit, market, close, open in zip(fxlst4,fxlst5,marketlst,close_id_lst,open_id_lst):
-    print(entry,exit,market,close,open)
+for entry, exit, market, close_id, open_id in zip(fxlst4,fxlst5,marketlst,close_id_lst,open_id_lst):
+    entry_yr = entry[0:4]
+    entry_mo = entry[5:7]
+    entry_day = entry[8:10]
+    entry_time = entry[11:]
+
+    exit_yr = exit[0:4]
+    exit_mo = exit[5:7]
+    exit_day = exit[8:10]
+    exit_time = exit[11:]
+
+    fxlog.append(
+                [entry, entry_yr, entry_mo, entry_day, entry_time,
+                exit, exit_yr, exit_mo, exit_day, exit_time,
+                market,
+                close_id, open_id]
+                )
+for log in fxlog:
+    print(log)
+print("\n-----------test of complete fxloglst----------------")
