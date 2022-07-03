fe0f8870dee506299c3d06815ee314eedf9a766c
Rashawn <arctitan78@gmail.com>
2022-07-03 12:03:37 -0400

    complete the functions for each data category; next compile the data from each list into a list, add the broker key, and create the unique transaction id

--- ib_dev_mode.py
+++ ib_dev_mode.py
@@ -45,7 +45,6 @@ def create_options_comm_fee_lst(lst1):
     for row in lst1[1:i]:
         options_lst1.append([row[5:9],row[10]])
         comm_fee_lst1.append(row[11])
-
 create_options_comm_fee_lst(trade_data_lst)
 
 # options_dates_lst contains the options dates
@@ -61,12 +60,13 @@ def extract_options_date(lst1,lst2):
             pass
         else:
             lst2.append(date[:10])
+    print(len(lst2))
 extract_options_date(options_lst1,options_dates_lst)
 
 # options_year_lst contains the options year
 options_year_lst = []
 
-# extract the YYYY from the options_dates_lst
+# extract the YYYY from options_dates_lst
 def extract_options_year(lst1, lst2):
     print("--------------TEST: extract_options_year()--------------------")
     i = len(lst1)
@@ -74,11 +74,13 @@ def extract_options_year(lst1, lst2):
         yr = row[:4]
         yr = int(yr)
         lst2.append(yr)
+    print(len(lst2))
 extract_options_year(options_dates_lst,options_year_lst)
 
 # options_month_lst contains the options month
 options_month_lst = []
 
+# extract the month from options_lst1
 def extract_options_month(lst1,lst2):
     print("-------------TEST: extract_options_month() func-----------------")
     i = len(lst1)
@@ -86,11 +88,13 @@ def extract_options_month(lst1,lst2):
         mo = row[5:7]
         mo = int(mo)
         lst2.append(mo)
+    print(len(lst2))
 extract_options_month(options_dates_lst,options_month_lst)
 
 # options_day_lst contains the options day
 options_day_lst = []
 
+# extract the transaction day from options_lst1
 def extract_options_day(lst1,lst2):
     print("-------------TEST: extract_options_day() func-----------------")
     i = len(lst1)
@@ -98,11 +102,13 @@ def extract_options_day(lst1,lst2):
         day = row[8:10]
         day = int(day)
         lst2.append(day)
+    print(len(lst2))
 extract_options_day(options_dates_lst,options_day_lst)
 
 # bought_sold_lst contains bought or sold options
 bought_sold_lst = []
 
+# add bought or sold
 def extract_options_bought_sold(lst1,lst2):
     for row in lst1:
         buy_sell = row[0][2]
@@ -117,20 +123,125 @@ def extract_options_bought_sold(lst1,lst2):
                     lst2.append('BOUGHT')
                 elif buy_sell <0:
                     lst2.append('SOLD')
+    print(len(lst2))
 extract_options_bought_sold(options_lst1,bought_sold_lst)
 
 # trade_size_lst contains the trade size
 trade_size_lst = []
 
+# extract the contract trade size from options_lst1
 def extract_options_trade_size(lst1,lst2):
     for row in lst1:
-        ctr = row[0][2]
-        if ctr == '':
+        num_ctr = row[0][2]
+        if num_ctr == '':
             pass
         else:
-            ctr = int(ctr)
-            if ctr == 0:
+            num_ctr = int(num_ctr)
+            if num_ctr == 0:
+                pass
+            else:
+                lst2.append(num_ctr)
+    print(len(lst2))
+extract_options_trade_size(options_lst1,trade_size_lst)
+
+# mkt_lst contains a list of the markets
+mkt_lst = []
+
+# extract the markets from options_lst1
+def extract_options_mkt(lst1,lst2):
+    print("-------------TEST: extract_options_mkt() func------------------")
+    for row in lst1:
+        if row[0][2] == str(0):
+            pass
+        else:
+            mkt = row[0][0][:3]
+            if mkt == '':
+                pass
+            elif mkt == str(0):
+                pass
+            else:
+                lst2.append(mkt)
+    print(len(lst2))
+extract_options_mkt(options_lst1,mkt_lst)
+
+# call_put_lst contains the calls and puts options contracts
+call_put_lst = []
+
+# extract the call / put data from options_lst1
+def extract_options_call_put(lst1,lst2):
+    print("------------TEST: extract_options_call_put() funct--------------")
+    for row in lst1:
+        if row[0][2] == str(0):
+            pass
+        elif row[0][0] == '':
+            pass
+        else:
+            call_put = row[0][0][-1]
+            if call_put == '':
+                pass
+            elif call_put == 'C' or call_put == 'CALL':
+                lst2.append(call_put)
+            elif call_put == 'P' or call_put == 'PUT':
+                lst2.append(call_put)
+    print(len(lst2))
+extract_options_call_put(options_lst1,call_put_lst)
+
+# ctr_lst contains a list of the contract details
+ctr_lst = []
+
+# extract the contract details from options_lst1
+def extract_options_ctr(lst1,lst2):
+    print("--------------TEST: extract_options_ctr() func----------------")
+    for row in lst1:
+        if row[0][2] == str(0):
+            pass
+        else:
+            ctr = row[0][0]
+            if ctr == '':
                 pass
             else:
                 lst2.append(ctr)
-extract_options_trade_size(options_lst1,trade_size_lst)
+    print(len(lst2))
+extract_options_ctr(options_lst1,ctr_lst)
+
+# price_lst contains the options price data
+price_lst = []
+
+# extract the price data from options_lst1
+def extract_options_price(lst1,lst2):
+    print("---------TEST: extract_options_price() func------------")
+    for row in lst1:
+        if row[0][2] == str(0):
+            pass
+        elif row[0][0] == '':
+            pass
+        else:
+            price = row[0][3]
+            price = float(price)
+            lst2.append(price)
+    print(len(lst2))
+extract_options_price(options_lst1,price_lst)
+
+# gross_lst contains the options gross amount data
+gross_lst = []
+
+# extract the gross amount from options_lst1
+def extract_options_gross(lst1,lst2):
+    print("-----------TEST: extract_options_gross()----------------")
+    for row in lst1:
+        if row[0][2] == str(0):
+            pass
+        elif row[0][0] == '':
+            pass
+        else:
+            price = row[1]
+            price = int(price)
+            lst2.append(price)
+    print(len(lst2))
+extract_options_gross(options_lst1,gross_lst)
+
+### START HERE NEXT ###
+# use the zip function to complie the data into a single list
+# add the broker id
+# add a function to create the trans_key
+# add the key to the list
