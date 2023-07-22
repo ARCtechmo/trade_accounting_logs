3e7e6d3eb01b70380300db6a0efae31cc34ee71d
ARCtechmo <arctitan78@gmail.com>
2023-07-22 10:04:03 -0400

    continued work on the gui

--- gui3.py
+++ gui3.py
@@ -44,7 +44,7 @@ menubar.add_cascade(label="Help", menu=helpMenu)
 helpMenu.add_command(label="About", command=menuItemSelected)
 helpMenu.add_command(label="Help", command=menuItemSelected)
 
-# function to query the database and display the results in a separate window
+# display all tables
 def display_tables():
 
     # connect to the database
@@ -52,24 +52,79 @@ def display_tables():
     cur = conn.cursor()
 
     # SQL statement to retrieve the table
-    # SQL = f'SELECT * FROM {table_name}'
     SQL = "SELECT name FROM sqlite_master WHERE type='table';"
 
     # Retrieve the table names using SQLite's PRAGMA statement
     with conn:
         cur.execute(SQL)
         table_names = [table[0] for table in cur.fetchall()]
-        # records = cur.fetchall()
-        # print_records = []
-        # for record in records:
-        #     print_records.append(str(record))
-      
+            
     # close the cursor and database connections
     cur.close()
     conn.close()
 
-    # return print_records
+    # return the tables
     return table_names
+
+# clear the previous query results before adding new ones
+def clear_results():
+    return print('------------------TEST: clear previous results---------------------')
+
+# function to query the database and display the results in a separate window
+def show_all_rows(table_name):
+
+    # connect to the database
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+
+    # clear the previous results
+    clear_results()
+
+    # create and name a new window 
+    window = Toplevel(root)
+    window.title(f'Show All Query Results for {table_name}')
+
+    # create the SQL statement
+    SQL = f"SELECT * FROM {table_name}"
+
+    # run the query and format the string output with one record on each line
+    with conn:
+        cur.execute(SQL)
+        records = cur.fetchall()
+        print_records = ''
+        for record in records:
+            print_records += str(record) + "\n"
+
+        # create a text widget to display the results
+        text_widget = Text(window, width=40, height=10)
+        text_widget.insert(END, print_records)
+        text_widget.grid(row=0,column=0, sticky=(N,S,E,W))
+
+        # create a scrollbar and associate it with the text widget
+        my_scrollbar = Scrollbar(window, command=text_widget.yview)
+        my_scrollbar.grid(row=0, column=1, sticky=((N,S)))
+        text_widget.config(yscrollcommand=my_scrollbar.set)
+    
+    # Configure the grid to expand with window resizing
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(0, weight=1)
+
+    # close the cursor and database connections
+    cur.close()
+    conn.close()
+
+# names of the query options
+query_names = {
+                'show all':'return all rows',
+                'by year': 'return by year'
+               }
+# variables
+query_name = StringVar()
+
+# function return the query values
+def run_query():
+    pass
+
 
 # create the frame for the tables and place on the grid
 tbl_frm = ttk.Frame(root, padding=(5,5,12,0))
@@ -80,21 +135,58 @@ root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0,weight=1)
 
 # create a listbox that contains the tables within the frame
-# table_items = ['table 1', 'table 2', 'table 3','table 4', 'table 5', 'table 6']
-# tables = StringVar(value=table_items) 
-# lbox = Listbox(tbl_frm, listvariable=tables, height=5)
-# tables = StringVar(value=display_tables('brokers'))
 tables = StringVar(value=display_tables())
 lbox = Listbox(tbl_frm, listvariable=tables, height=15, width=30)
-lbox.grid(column=0, row=0, rowspan=10, sticky=(N,S,E,W))
 
 # create a vertical scrollbar
 scrollbar = Scrollbar(tbl_frm, orient=VERTICAL)
-scrollbar.grid(column=1, row=0, rowspan=6, sticky=(N,S))
 
 # link the scrollbar to the listbox
 lbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=lbox.yview)
+
+# create a label for the query options and place on the grid
+lbl = ttk.Label(tbl_frm, text="Select a query option:")
+
+# create the radio buttons for the query options
+b1 = ttk.Radiobutton(tbl_frm, 
+                     text=query_names["show all"],
+                     variable=query_name,
+                     value='show all'
+                     )  
+
+b2 = ttk.Radiobutton(tbl_frm, 
+                     text=query_names["by year"],
+                     variable=query_name,
+                     value='by year'
+                     )  
+
+# create a button to run the query 
+run_query_btn = ttk.Button(tbl_frm, 
+                         text='Run Query',
+                         command=show_all_rows,
+                         default='active' )
+
+
+
+# grid all the widgets
+lbox.grid(column=0, row=0, rowspan=6, sticky=(N,S,E,W))
+scrollbar.grid(column=1, row=0, rowspan=6, sticky=(N,S))
+lbl.grid(column=2, row=1, padx=10, pady=5)
+b1.grid(column=2, row=2, sticky=W, padx=20)
+b2.grid(column=2, row=3, sticky=W, padx=20)
+run_query_btn.grid(column=2, row=5, sticky=E, padx=5, pady=5)
+
+# frame expands as the window expands and fixes row in place  
+tbl_frm.grid_columnconfigure(0, weight=1)
+tbl_frm.grid_rowconfigure(5, weight=1)
+
+# set event bindings for when the selection in the listbox changes,
+# when the user double clicks the list, and when they hit the Return key
+lbox.bind('<<ListboxSelect>>',show_all_rows)
+lbox.bind('<Double-1>', show_all_rows)
+root.bind('<Return>', show_all_rows)
+
 
 # create the main loop of the program
 root.mainloop()
